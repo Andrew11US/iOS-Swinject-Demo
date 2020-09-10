3ecba85eae378fb2297dc7dbaf6a3d269563cbf6
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-09-10 22:55:36 +0200

    Initial Commit

---
+++ Swinject Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swinject Demo
+//
+//  Created by Andrew on 9/10/20.
+//  Copyright © 2020 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

13c2d6a723d1a3ddd00c0690c8fb409fffdcece5
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-09-10 23:18:19 +0200

    Completed demo project using Swinject

--- Swinject Demo/ViewController.swift
+++ Swinject Demo/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import Swinject
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let container = Container()
+        container.register(NetworkServicing.self) { _ in NetworkService() }
+        container.register(ViewModel.self) { resolver in
+            let viewModel = ViewModel()
+            viewModel.service = resolver.resolve(NetworkServicing.self)
+            return viewModel
+        }
+        
+        let viewModel: ViewModel? = container.resolve(ViewModel.self)
+        viewModel?.loadData()
     }
 
 
+}
+
+class ViewModel {
+    var service: NetworkServicing!
+    
+    func loadData() {
+        service.getData()
+    }
+}
+
+struct NetworkService: NetworkServicing {
+    func getData() {
+        print("Data has been fetched")
+    }
+}
+
+protocol NetworkServicing {
+    func getData()
 }
 
